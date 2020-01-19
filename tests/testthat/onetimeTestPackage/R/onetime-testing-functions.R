00570b9a0871717ae21f9b7526e967ee27da6d3f
David Hugh-Jones <davidhughjones@gmail.com>
2020-01-19 19:05:54 +0000

    test calling from external package

---
+++ tests/testthat/onetimeTestPackage/R/onetime-testing-functions.R
@@ -0,0 +1,7 @@
+
+
+test_onetime_do <- function (...) onetime::onetime_do(...)
+
+test_onetime_echo <- function (x) onetime::onetime_do(cat(x))
+
+test_onetime_reset <- function (...) onetime::onetime_reset(...)
---
+++ tests/testthat/test-external-package.R
@@ -0,0 +1,37 @@
+
+setup({
+  withr::with_libpaths("onetimeTestLibrary", {
+    devtools::install("onetimeTestPackage",
+          args = c(quiet = TRUE, verbose = FALSE))
+    library(onetimeTestPackage)
+  })
+})
+
+
+test_that("Calling from external package", {
+
+  expect_output(test_onetime_echo("xxx"), "xxx")
+  expect_silent(test_onetime_echo("xxx"))
+
+
+  ctr <- 0
+  test_onetime_do(ctr <- ctr + 1, id = "foo")
+  expect_equal(ctr, 1)
+  test_onetime_do(ctr <- ctr + 1, id = "foo")
+  expect_equal(ctr, 1)
+
+  test_onetime_reset("foo")
+  test_onetime_do(ctr <- ctr + 1, id = "foo")
+  expect_equal(ctr, 2)
+  test_onetime_do(ctr <- ctr + 1, id = "foo")
+  expect_equal(ctr, 2)
+})
+
+
+teardown({
+  test_onetime_reset()
+  test_onetime_reset("foo")
+  withr::with_libpaths("onetimeTestLibrary", {
+    remove.packages("onetimeTestPackage")
+  })
+})

2cc2972c4140dc410bfce6f7bf6dc787b30ad09e
David Hugh-Jones <davidhughjones@gmail.com>
2020-01-21 19:19:18 +0000

    make system2() calls portable in test

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,5 +1,9 @@
 
 
+R_path <- file.path(R.home("bin"), "R")
+
+
+
 test_that(".onLoad", {
   unloadNamespace(getNamespace("onetime"))
   lfd <- file.path(rappdirs::user_config_dir(), "onetime-lockfiles")
@@ -50,13 +54,13 @@ test_that("onetime_reset", {
 
 
 test_that("multiprocess and command line", {
-  x <- system2("R", c("-q", "-e",
+  x <- system2(R_path, c("-q", "-e",
         "'onetime::onetime_do(cat(\"foo\\n\"), id = \"test-id-7\")'"),
         stdout = TRUE
       )
   expect_match(x, "^foo$", perl = TRUE, all = FALSE)
 
-  x <- system2("R", c("-q", "-e",
+  x <- system2(R_path, c("-q", "-e",
           "'onetime::onetime_do(cat(\"foo\\n\"), id = \"test-id-7\")'"),
           stdout = TRUE
         )
@@ -67,6 +71,6 @@ teardown({
   for (test_id in paste0("test-id-", 1:6)) {
     onetime_reset(test_id)
   }
-  system2("R", c("-q", "-e", "'onetime::onetime_reset(id = \"test-id-7\")'"),
+  system2(R_path, c("-q", "-e", "'onetime::onetime_reset(id = \"test-id-7\")'"),
         stdout = FALSE)
 })
